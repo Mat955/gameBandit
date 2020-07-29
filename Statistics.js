58154971fa099b8cd5dd34be83e23cc4fd964018
unknown <lewar2013c@gmail.com>
2020-07-29 13:29:10 +0200

    added class of draw,result and statistics

---
+++ Statistics.js
@@ -0,0 +1,23 @@
+class Statistics {
+  constructor() {
+    this.gameResults = [];
+  }
+
+  addGameToStatistics(win, bid) {
+    let gameResults = {
+      win,
+      bid
+    }
+    console.log(gameResults);
+    this.gameResults.push(gameResults)
+  }
+
+  showGameStatistics() {
+    let games = this.gameResults.length;
+    let wins = this.gameResults.filter(gameResult => gameResult.win).length;
+    let losses = this.gameResults.filter(gameResult => !gameResult.win).length;
+    return [games, wins, losses];
+  }
+}
+
+const stats = new Statistics()
